085727efc05f7ddba3a635c1b7ccb240a9dca4ed
osamakhanedu <osamakhanedu@gmail.com>
2026-05-16 12:42:09 +0400

    ruby step 2

--- intro/hello1.rb
+++ intro/hello1.rb
@@ -1,6 +1,6 @@
+# comment: Ruby return the last expression if return is keyword is not there.
 def say_hello_goodbye(name)
-    result = "I don't know why you say goodbye, " + name + ", i say hello"
-    return result
+    "I don't know why you say goodbye, #{name}, i say hello"
 end 
 
 puts say_hello_goodbye("jhon")
