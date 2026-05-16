036f143bfac977aec95e0b5f775c3ff58da2f543
osamakhanedu <osamakhanedu@gmail.com>
2026-05-16 12:32:10 +0400

    Learning ruby

---
+++ intro/hello1.rb
@@ -0,0 +1,8 @@
+def say_hello_goodbye(name)
+    result = "I don't know why you say goodbye, " + name + ", i say hello"
+    return result
+end 
+
+puts say_hello_goodbye("jhon")
+puts say_hello_goodbye("paul")
+
